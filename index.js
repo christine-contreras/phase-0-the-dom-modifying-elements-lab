bbfa9771fb4042845a604d2b00ed4b896539d9b1
Christine Contreras <christineec23@yahoo.com>
2021-03-15 14:11:41 -0500

    completed dom

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+// no longer has DOM node 'main#main':
+document.getElementById('main').remove();
+
+//create element
+const newHeader = document.createElement('h1');
+
+//add ID to element 
+newHeader.setAttribute('id', 'victory');
+
+//add element to body
+document.body.appendChild(newHeader);
+
+//add inner text to element 
+newHeader.innerHTML = "YOUR-NAME is the champion";
